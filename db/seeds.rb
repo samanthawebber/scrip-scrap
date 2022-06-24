c5c06eae2839dd7bec34d0309aa904f59fbbbb33
Samantha <samantha.webber@protonmail.com>
2022-06-25 00:20:34 +0200

    Create model and controller for Posts, seed database with a couple posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create(title: "Engineered Hardwood Bamboo", description: "Three full unopened boxes.", location: "Palm Coast, FL")
