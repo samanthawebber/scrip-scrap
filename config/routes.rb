362ceaa92a2bc8584a1ab1d62ebc317887d9c623
Samantha <samantha.webber@protonmail.com>
2022-07-05 16:27:17 +0200

    React stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :posts
+  namespace :api do
+    namespace :v1 do
+      get 'posts/index'
+      post 'posts/create'
+      delete 'posts/:id', to: 'posts#destroy'
+    end
+  end
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
